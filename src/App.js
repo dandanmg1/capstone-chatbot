68d5e0a85542eedcf08f7353b8dd383afd726fb8
Danny Jung <dkmjung1@gmail.com>
2023-11-19 17:38:37 -0800

    update backend features

--- src/App.js
+++ src/App.js
@@ -1,106 +1,140 @@
-import { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react';
 
-const App = () => {
-    const [value, setValue] = useState (null)
-    const [message, setMessage]  = useState(null)
-    const [previousChats, setPreviousChats] = useState([])
-    const [currentTitle, setCurrentTitle] = useState(null)
+// const App = () => {
+//   const [value, setValue] = useState('');
+//   const [message, setMessage] = useState(null);
+//   const [previousChats, setPreviousChats] = useState([]);
+//   const [currentTitle, setCurrentTitle] = useState(null);
 
-    const createNewChat = () => {
-      setMessage(null)
-      setValue("")
-      setCurrentTitle(null)
-    }
+//   const createNewChat = () => {
+//     setMessage(null);
+//     setValue('');
+//     setCurrentTitle(null);
+//   };
 
-    const handleClick = (uniqueTitle) => {
-      setCurrentTitle(uniqueTitle)
-      setMessage(null)
-      setValue("")
-    }
+//   const handleClick = (uniqueTitle) => {
+//     setCurrentTitle(uniqueTitle);
+//     setMessage(null);
+//     setValue('');
+//   };
 
-    const getMessages = async () => {
-      const options = {
-        method: "POST",
-        body: JSON.stringify({
-          message: value
-        }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }
-      try {
-        const response = await fetch('https://localhost:8000/completions', options)
-        const data = await response.json()
-        setMessage(data.choices[0].message)
-      } catch (error) {
-          console.error(error)
-      }
-    }
+//   const getMessages = async () => {
+//     const options = {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         message: value,
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     };
 
-  
-  useEffect(() => {
-    console.log(currentTitle, value, message)
-    if (!currentTitle && value && message) {
-        setCurrentTitle(value)
-    }
-    if (currentTitle && value && message) {
-        setPreviousChats(prevChats => (
-          [...prevChats, 
-              {
-                title: currentTitle,
-                role: "user",
-                content: value
-              }, 
-              {
-                title: currentTitle,
-                role: message.role,
-                content: message.content
-              }
-          ]
-        ))
-    }
-  }, [message, currentTitle])
+//     try {
+//       const response = await fetch('http://localhost:8000/completions', options);
+//       const data = await response.json();
 
-  console.log(previousChats)
+//       console.log('OpenAI API Response:', data);
 
-  const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
-  const uniqueTitles = Array.from (new Set(previousChats.map(previousChat => previousChat.title)))
-  console.log(uniqueTitles)
-  
-  return (
-    <div className="app">
-      <section className="side-bar">
-        <button onClick={createNewChat}>+ New Chat</button>
-        <ul className="history">
-          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick}>{uniqueTitle}</li>)}
-        </ul>
-        <nav>
-          <p>Made by Innergy</p>
-        </nav>
-      </section>
-      <section className="main">
-        {!currentTitle && <h1>InnergyGPT</h1>}
-        <ul className="feed">
-          {currentChat?.map((chatMessage, index) => <li key={index}>
-            <p className="role">{chatMessage.role}</p>
-            <p>{chatMessage.content}</p>
-          </li>)}
-        </ul>
-        <div className="bottom-section">
-          <div className="input-container">
-            <input value={value} onChange={(e) => setValue(e.target.value)}/>
-            <div id="submit" onClick={getMessages}>➢</div>
-          </div>
-          <p className="info">
-            Chat GPT Mar 14 Version.  Free Research Preview. 
-            Our goal is to make AI systems more natural and safe to interact with.
-            Your feedback will help us improve.
-          </p>
+//       if (data.error) {
+//         return window.alert('Please try again later');
+//       }
 
-        </div>
-      </section>
-    </div>
-  )
-}
+//       // Extract the generated message from the API response
+//       const generatedMessage = data.choices[0].message.content;
 
-export default App
+//       // Update the state with the generated message
+//       setMessage({
+//         role: 'AI', // Assuming 'AI' as the role for generated messages
+//         content: generatedMessage,
+//       });
+
+//       // Update the previous chats state with the user's input and the generated message
+//       setPreviousChats((prevChats) => [
+//         ...prevChats,
+//         {
+//           title: currentTitle,
+//           role: 'user',
+//           content: value,
+//         },
+//         {
+//           title: currentTitle,
+//           role: 'AI',
+//           content: generatedMessage,
+//         },
+//       ]);
+
+//       // Clear the input field
+//       setValue('');
+
+//     } catch (error) {
+//       console.error('Error in fetch:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (!currentTitle && value && message) {
+//       setCurrentTitle(value);
+//     }
+//     if (currentTitle && value && message) {
+//       setPreviousChats((prevChats) => [
+//         ...prevChats,
+//         {
+//           title: currentTitle,
+//           role: 'user',
+//           content: value,
+//         },
+//         {
+//           title: currentTitle,
+//           role: 'AI',
+//           content: message.content,
+//         },
+//       ]);
+//     }
+//   }, [message, currentTitle, value]);
+
+//   const currentChat = previousChats.filter((chatMessage) => chatMessage.title === currentTitle);
+//   const uniqueTitles = Array.from(new Set(previousChats.map((previousChat) => previousChat.title)));
+
+//   return (
+//     <div className="app">
+//       <section className="side-bar">
+//         <button onClick={createNewChat}>+ New Chat</button>
+//         <ul className="history">
+//           {uniqueTitles?.map((uniqueTitle, index) => (
+//             <li key={index} onClick={() => handleClick(uniqueTitle)}>
+//               {uniqueTitle}
+//             </li>
+//           ))}
+//         </ul>
+//         <nav>
+//           <p>Made by Innergy</p>
+//         </nav>
+//       </section>
+//       <section className="main">
+//         {!currentTitle && <h1>InnergyGPT</h1>}
+//         <ul className="feed">
+//           {currentChat?.map((chatMessage, index) => (
+//             <li key={index}>
+//               <p className="role">{chatMessage.role}</p>
+//               <p>{chatMessage.content}</p>
+//             </li>
+//           ))}
+//         </ul>
+//         <div className="bottom-section">
+//           <div className="input-container">
+//             <input value={value} onChange={(e) => setValue(e.target.value)} />
+//             <div id="submit" onClick={getMessages}>
+//               ➢
+//             </div>
+//           </div>
+//           <p className="info">
+//             Chat GPT Mar 14 Version. Free Research Preview. Our goal is to make AI systems more natural and safe to interact with.
+//             Your feedback will help us improve.
+//           </p>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/server.js
+++ src/server.js
@@ -1,32 +1,33 @@
-const PORT = 8000
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(express.json())
-app.use(cors())
+const OpenAI = require ('openai');
+  
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config()
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+  });
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const API_KEY = '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'
+const API_KEY = process.env.API_KEY;
 
-app.post('/completion', async (req,res) => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: req.body.message}],
-            max_tokens: 100,
-        })
-    }
+app.post('/completions', async (req, res) => {
     try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-        const data = await response.json() 
-        res.send(data)
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: req.body.message }],
+            model: "gpt-3.5-turbo",
+            max_tokens: 100,
+          });
+        console.log('test',chatCompletion)
+    
     } catch (error) {
-        console.error(error)
+        console.error('Error in /completions:', error.message);
+        res.status(500).send({ error: 'Internal Server Error' });
     }
-})
+});
 
-app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
+const server = require('http').createServer(app);
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
